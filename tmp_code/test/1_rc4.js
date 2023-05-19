546f725c5d658ed8a549dc73ebe791aca8a40c72
Anders <anders@emojidao.org>
2023-05-19 23:54:24 +0800

    add test code for RC4.sol

--- tmp_code/test/1_rc4.js
+++ tmp_code/test/1_rc4.js
@@ -1,8 +1,8 @@
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 const { BigNumber } = require("bignumber.js")
 const Web3 = require("web3");
 const web3 = new Web3();
-
+const rc4js = require('rc4-cipher');
 const RC4 = artifacts.require("RC4");
 
 contract("test RC4", async accounts => {
@@ -11,17 +11,29 @@ contract("test RC4", async accounts => {
 
         const rc4 = await RC4.deployed();
 
-        let input = web3.utils.asciiToHex("input");
-        input = web3.eth.abi.encodeParameter("bytes32", input);
+        let input = "input";
+        let inputHex=  web3.utils.asciiToHex(input);
+        let inputByte32 =   web3.eth.abi.encodeParameter("bytes32", inputHex);
+        let inputString =  web3.utils.hexToAscii(inputByte32);
 
-        let key = web3.utils.asciiToHex("key");
-        key = web3.eth.abi.encodeParameter("bytes32", key);
-        console.log({input}, {key});
+        let key = "key";
+        let keyHex = web3.utils.asciiToHex(key);
+        let keyByte32 =   web3.eth.abi.encodeParameter("bytes32", keyHex);
+        let keyString = web3.utils.hexToAscii(keyByte32);
 
 
-        let  output = await rc4.rc4_encrypt_decrypt_bytes32(input,key);
+        let  output1 = await rc4.encryptBytes32(inputByte32,keyByte32);
 
-        console.log("output: ", output);
+        let output2 =  rc4js.encrypt(inputString,keyString);
+        output2 = web3.utils.asciiToHex(output2);
+
+        expect(output1).to.equal(output2);
+
+        let  output3 = await rc4.encryptBytes32(output1,keyByte32);
+        expect(output3).to.equal(inputByte32);
+
+
+        console.log({input,inputByte32, inputString, key, keyByte32, keyString, output1, output2});
     });
     
 
@@ -29,17 +41,50 @@ contract("test RC4", async accounts => {
 
         const rc4 = await RC4.deployed();
 
-        let input = web3.utils.asciiToHex("input");
+        let input = "input";
+        let inputHex=  web3.utils.asciiToHex(input);
 
-        let key = web3.utils.asciiToHex("key");
+        let key = "key";
+        let keyHex = web3.utils.asciiToHex(key);
 
-        console.log({input}, {key});
         
-        let  output = await rc4.rc4_encrypt_decrypt_bytes(input,key);
+        let output1 = await rc4.encryptBytes(inputHex,keyHex);
+        let output2 =  rc4js.encrypt(input,key);
+        output2 = web3.utils.asciiToHex(output2);
 
-        console.log("output: ", output);
+        expect(output1).to.equal(output2);
+
+        console.log({input,key,output1, output2});
+
+        let  output3 = await rc4.encryptBytes(output1,keyHex);
+        expect(output3).to.equal(inputHex);
+
     });
 
-    
+    it("test js encrypt", async () => {
 
+       
+        // data example from contract
+        let inputByte32=  "0x0000000000000000000000000000000000000000000000000000000000000001";
+       
+        let inputString =  web3.utils.hexToAscii(inputByte32);
+        
+
+        let key = "key";
+        let keyHex = web3.utils.asciiToHex(key);
+        let keyByte32 =   web3.eth.abi.encodeParameter("bytes32", keyHex);
+        let keyString = web3.utils.hexToAscii(keyByte32);
+        
+        let output1 =  rc4js.encrypt(inputString,keyString);
+        let output1Hex = web3.utils.asciiToHex(output1);
+        
+        // send output1Byte32 to contract
+        let output1Byte32 =   web3.eth.abi.encodeParameter("bytes32", output1Hex);
+
+        let output2 = rc4js.encrypt(output1,keyString);
+
+        expect(output2).to.equal(inputString);
+
+        console.log({inputByte32,inputString, key,keyHex, keyByte32, keyString, output1,output1Byte32, output2});
+    }); 
 });
