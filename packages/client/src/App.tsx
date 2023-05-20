670a1ba82da3c4b19ecdcb18a15b41b3820aa6ba
yyg <yangyongguan@weimiao.cn>
2023-05-20 21:21:48 +0800

    create game

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,28 +1,30 @@
 import { useComponentValue } from "@latticexyz/react";
 import { useMUD } from "./MUDContext";
+import { ethers } from "ethers";
 
 export const App = () => {
   const {
-    components: { Counter },
-    systemCalls: { increment },
+    components: { Game },
+    systemCalls: { createGame },
     network: { singletonEntity },
   } = useMUD();
 
-  const counter = useComponentValue(Counter, singletonEntity);
-
+  const game = useComponentValue(Game, singletonEntity);
+  console.log('game-----')
+  console.log(game)
   return (
     <>
       <div>
-        Counter: <span>{counter?.value ?? "??"}</span>
+        Counter: <span>{game?.cardHash}</span>
       </div>
       <button
         type="button"
         onClick={async (event) => {
           event.preventDefault();
-          console.log("new counter value:", await increment());
-        }}
-      >
-        Increment
+          const byte32GameId = ethers.utils.formatBytes32String('100');
+          console.log("new counter value:", await createGame(byte32GameId));
+        }}>
+          createGame
       </button>
     </>
   );
