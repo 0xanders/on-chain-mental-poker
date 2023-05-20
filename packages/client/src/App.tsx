6b1bf8d8a3c29a0b6f0322c3ae16ac375026b448
yyg <yangyongguan@weimiao.cn>
2023-05-20 23:38:01 +0800

    create game

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,6 +4,34 @@ import './app.css';
 import { world } from "./mud/world";
 import {ethers} from "ethers";
 import {useState} from "react";
+enum GameState {
+    Join,
+    Shuffle,
+    DealCards,
+    DecryptForOthers,
+    UploadSecret,
+    Error,
+    Finished
+}
+const getGameState = (state: number) => {
+    switch (state) {
+        case GameState.Join:
+            return 'Join'
+        case GameState.Shuffle:
+            return 'Shuffle'
+        case GameState.DealCards:
+            return 'DealCards'
+        case GameState.DecryptForOthers:
+            return 'DecryptForOthers'
+        case GameState.UploadSecret:
+            return 'UploadSecret'
+        case GameState.Error:
+            return 'Error'
+        case GameState.Finished:
+            return 'Finished'
+        return 'Join'
+    }
+}
 export const App = () => {
   const {
     components: { Game },
@@ -23,19 +51,21 @@ export const App = () => {
           createGame(gameId)
       }
   }
-  return (
+    return (
     <>
-      <div>
-          gameState: <span>{game?.state}</span>
-          singletonEntity: <span></span>
-      </div>
+      <ul className={'game-warp'}>
+          <li>state: <span>{game?.state}({getGameState(game?.state || 0)})</span></li>
+          <li>turn: <span>{game?.turn}</span></li>
+          <li>cardIndex: <span>{game?.cardIndex}</span></li>
+          <li>winner: <span>{game?.winner}</span></li>
+          <li>cardsHash: <span>{game?.cardsHash}</span></li>
+          <li>players: <span>{game?.players}</span></li>
+      </ul>
         <div className={'form'}>
             <input placeholder={'Please enter gameID'} onChange={(e: any) => {
                 setGameId(e.target.value || '')
             }}/>
-            <div className={'button-warp'} onClick={createOrJoinGame}>
-                <button>{game ? 'Join Game' : 'Create Game'}</button>
-            </div>
+            <button onClick={createOrJoinGame}>{game ? 'Join Game' : 'Create Game'}</button>
         </div>
     </>
   );
