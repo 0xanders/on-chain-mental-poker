dcd2be1d407fdfb2f1aad6faf18d1865f37700a4
LanceSnow <lance@emojidao.org>
2023-05-21 11:36:38 +0800

    test shuffleAndSave

--- packages/contracts/test/GameTest.t.sol
+++ packages/contracts/test/GameTest.t.sol
@@ -2,13 +2,21 @@
 pragma solidity >=0.8.0;
 
 import "forge-std/Test.sol";
+import { console } from "forge-std/console.sol";
 import { MudV2Test } from "@latticexyz/std-contracts/src/test/MudV2Test.t.sol";
 import { getKeysWithValue } from "@latticexyz/world/src/modules/keyswithvalue/getKeysWithValue.sol";
 
 import { IWorld } from "../src/codegen/world/IWorld.sol";
+import { Game, GameData } from "../src/codegen/tables/Game.sol";
+import { Commitment, CommitmentData } from "../src/codegen/tables/Commitment.sol";
+import { HandCard, HandCardData } from "../src/codegen/tables/HandCard.sol";
+import { GameState } from "../src/codegen/Types.sol";
 
 contract GameTest is MudV2Test {
   IWorld public world;
+  address public Alice = address(0x001);
+  address public Bob = address(0x002);
+  address public Carl = address(0x003);
 
   function setUp() public override {
     super.setUp();
@@ -25,16 +33,63 @@ contract GameTest is MudV2Test {
   }
 
   function testCreateGame() public {
-    world.createGame("0x1");
+    vm.startPrank(Alice);
+    bytes32 gameId = bytes32("0x1");
+    world.createGame(gameId);
+
+    // assertEq(Game.getState(world, gameId), GameState.Join);
 
     vm.expectRevert("Invalid Player");
-    world.joinInGame("0x1");
-    
-    address alice = address(1234);
-    vm.startPrank(alice);
-    // behave as alice
-    world.joinInGame("0x1");
+    world.joinInGame(gameId);
     vm.stopPrank();
 
+    vm.startPrank(Bob);
+    world.joinInGame(gameId);
+    vm.stopPrank();
+
+    vm.startPrank(Carl);
+    world.joinInGame(gameId);
+    vm.stopPrank();
+
+    // assertEq(Game.getState(world, gameId), GameState.Shuffle);
+
+    address[] memory players = Game.getPlayers(world, gameId);
+    assertEq(players[0], Alice);
+    assertEq(players[1], Bob);
+    assertEq(players[2], Carl);
+
+    //----------GameState.Shuffle--------
+    bytes32 msgToSign = "0xPoker";
+    bytes32 resultOfSign;
+    bytes32 secretKey_Alice = "0xAlice";
+    bytes32 secretKey_Bob = "0xBob";
+    bytes32 secretKey_Carl = "0xCarl";
+    bytes32[52] memory cardsHash;
+    bytes32[52] memory cards = world.getCards();
+
+    vm.startPrank(Alice);
+    for (uint i = 0; i < cards.length; i++) {
+      cardsHash[i] = world.rc4EncryptBytes32(cards[i], secretKey_Alice);
+    }
+    resultOfSign = world.rc4EncryptBytes32(msgToSign, secretKey_Alice);
+    world.shuffleAndSave(gameId, msgToSign, resultOfSign, cardsHash);
+    vm.stopPrank();
+
+    vm.startPrank(Bob);
+    for (uint i = 0; i < cardsHash.length; i++) {
+      cardsHash[i] = world.rc4EncryptBytes32(cardsHash[i], secretKey_Bob);
+    }
+
+    resultOfSign = world.rc4EncryptBytes32(msgToSign, secretKey_Bob);
+    world.shuffleAndSave(gameId, msgToSign, resultOfSign, cardsHash);
+    vm.stopPrank();
+
+    vm.startPrank(Carl);
+    for (uint i = 0; i < cardsHash.length; i++) {
+      cardsHash[i] = world.rc4EncryptBytes32(cardsHash[i], secretKey_Carl);
+    }
+    resultOfSign = world.rc4EncryptBytes32(msgToSign, secretKey_Carl);
+    world.shuffleAndSave(gameId, msgToSign, resultOfSign, cardsHash);
+    vm.stopPrank();
   }
 }
