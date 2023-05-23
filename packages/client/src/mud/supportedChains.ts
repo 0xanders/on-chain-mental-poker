b74a0e459f91e36719e40ed88e61fb9a3033f3c2
LanceSnow <lance@emojidao.org>
2023-05-23 19:18:46 +0800

    local test

--- packages/client/src/mud/supportedChains.ts
+++ packages/client/src/mud/supportedChains.ts
@@ -3,4 +3,7 @@ import { foundry } from "@wagmi/chains";
 import { doubleTestnet } from "./doubleTestnet";
 
 // If you are deploying to chains other than anvil or Lattice testnet, add them here
-export const supportedChains: MUDChain[] = [latticeTestnet, doubleTestnet];
+// local test
+export const supportedChains: MUDChain[] = [foundry, doubleTestnet];
+// doubleTest
+// export const supportedChains: MUDChain[] = [latticeTestnet, doubleTestnet];
